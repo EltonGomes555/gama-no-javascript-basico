6e08b3278b51da0102d2a607ad7779865d0ccf6c
Elton Gomes <elton55_55@hotmail.com>
2020-11-29 22:47:02 -0300

    add

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("Javascript carregado");
+
+function validaCPF(cpf) {
+   if (cpf.lenght != 11) {
+      return false;
+   } else {
+      var numeros = cpf.substring(0, 9);
+      var digitos = cpf.substring(9);
+
+
+      var soma = 0;
+      for (var i = 10; i > 1; i --) {
+         soma += numeros.charAt(10 - i) * i;
+      }
+       console.log(soma);  
+
+      return true;
+   }
+
+}
+
+function validacao() {
+   console.log("Iniciando validação CPF");
+
+   var cpf = document.getElementById("cpf_digitado").value;
+
+   var resultadoValidacao = validaCPF(cpf);
+
+   if (resultadoValidacao) {
+      document.getElementById("success").style.display = "block";
+
+   } else {
+      document.getElementById("error").style.display = "block";
+
+   }
+
+}
